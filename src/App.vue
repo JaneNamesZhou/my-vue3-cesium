da9a7569c8a763e79b9e380e149a38d2466881f0
unknown <897565440@qq.com>
2025-06-21 09:43:49 +0800

    commit first project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="cesiumContainer"></div>
+</template>
+
+<script setup>
+  import * as Cesium from 'cesium'
+  import "./Widgets/widgets.css"
+  import { onMounted } from 'vue';
+
+  window.CESIUM_BASE_URL = "/";
+
+  onMounted(()=>{
+    new Cesium.Viewer("cesiumContainer");
+  })
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+</style>
